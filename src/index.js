87ced73eb18a8520583d40080b6bfc8adc9d030b
mustafasen <mustafasen@tuta.io>
2023-01-23 17:07:15 +0300

    added cors middleware

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 const express = require('express')
+const cors = require('cors')
 
-// TODO: create process env for port
 const PORT = process.env.PORT;
 const app = express();
 const visitRouter = require('./routers/visits')
 
+
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200
+}
+
 app.use(express.json())
 app.use(visitRouter)
+app.use(cors(corsOptions))
 
 app.listen(PORT, () => {
     console.log(`Listening from port: ${PORT}`)
